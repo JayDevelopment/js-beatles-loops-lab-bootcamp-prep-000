58f55245706427980cada45cfc1553c838d8b04a
Jordan Merrill <amanithought@gmail.com>
2018-03-20 14:55:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function theBeatlesPlay (players, instruments) {
   return allPlayers
 }
 function johnLennonFacts () {
-  const facts = [
+  var facts = [
   "He was the last Beatle to learn to drive",
   "He was never a vegetarian",
   "He was a choir boy and boy scout",

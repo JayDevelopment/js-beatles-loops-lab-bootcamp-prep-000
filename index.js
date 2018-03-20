9c4e678e49d7fce1486c1aca8bb24e003e32294e
Jordan Merrill <amanithought@gmail.com>
2018-03-20 13:32:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay (players, instruments) {
   var allPlayers = []
   for (var i=0; i < players.length; i++) {
-    allPlayers.push(players)
+    allPlayers.push(players[i])
   }
 }
